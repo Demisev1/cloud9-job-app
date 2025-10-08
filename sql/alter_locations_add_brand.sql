a94c3f6bd96fb441daa7eacb17881bd8785d9141
Demisev1 <130713230+Demisev1@users.noreply.github.com>
2025-10-07 19:45:57 -0500

    Delete sql directory

--- sql/alter_locations_add_brand.sql
+++
@@ -1,5 +0,0 @@
--- Adds a brand field to locations: 'Cloud 9 Vapes' or 'The Hemp and Kratom Depot'
-alter table public.locations
-add column if not exists brand text
-  check (brand in ('Cloud 9 Vapes', 'The Hemp and Kratom Depot'))
-  default 'Cloud 9 Vapes';
--- sql/seed_locations_brand.sql
+++
@@ -1,3 +0,0 @@
--- Example updates for your current stores (edit if names differ)
-update public.locations set brand = 'Cloud 9 Vapes' where store_name ilike '%Cloud 9%';
-update public.locations set brand = 'The Hemp and Kratom Depot' where store_name ilike '%Kratom Depot%';
