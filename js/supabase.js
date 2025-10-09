a0db10f1ac182e072422e33ad6347dbbc99e5763
Demisev1 <130713230+Demisev1@users.noreply.github.com>
2025-10-09 00:00:34 -0500

    Delete js directory

--- js/supabase.js
+++
@@ -1,3 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
-import { SUPABASE_URL, SUPABASE_ANON } from "./config.js";
-export const sb = createClient(SUPABASE_URL, SUPABASE_ANON);
